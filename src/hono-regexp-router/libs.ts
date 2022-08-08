7a894dec0ed956ef4afc2a355f32746e30d77a03
saltyaom <saltyaom@gmail.com>
2022-08-08 23:09:04 +0700

    fix(router): parse port

--- src/hono-regexp-router/libs.ts
+++ src/hono-regexp-router/libs.ts
@@ -1,16 +1,16 @@
-const hostRegex = /^\w+:.*?(:)\d*/g
+const hostRegex = /^\w+:\/\/.*?\//g
 const paramsRegex = /([^?=&]+)(=([^&]*))?/g
 
 export const parseUrl = (url: string) => {
-	const [pathname, search] = url.replace(hostRegex, '').split('?')
+    const [pathname, search] = url.replace(hostRegex, '/').split('?')
 
-	return [pathname, search]
+    return [pathname, search]
 }
 
 export const parseQuery = (search: string) =>
-	(search.match(paramsRegex) || []).reduce((result, each) => {
-		const [key, value] = each.split('=')
-		result[key] = value
+    (search.match(paramsRegex) || []).reduce((result, each) => {
+        const [key, value] = each.split('=')
+        result[key] = value
 
-		return result
-	}, {} as Record<string, string>)
+        return result
+    }, {} as Record<string, string>)
