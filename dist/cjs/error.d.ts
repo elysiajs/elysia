3396ec66bc95fffb995a17301c4bb9f26df59d32
saltyaom <saltyaom@gmail.com>
2022-12-08 21:39:42 +0700

    :wrench: fix: rc.2

--- dist/cjs/error.d.ts
+++
@@ -1,3 +0,0 @@
-import type { ErrorCode } from './types';
-export declare const mapErrorCode: (error: string) => ErrorCode;
-export declare const mapErrorStatus: (error: string) => number;
--- dist/error.js
+++
@@ -1,8 +0,0 @@
-const errorCodeToStatus = new Map();
-errorCodeToStatus.set('BODY_LIMIT', 400);
-errorCodeToStatus.set('INTERNAL_SERVER_ERROR', 500);
-errorCodeToStatus.set('NOT_FOUND', 404);
-errorCodeToStatus.set('VALIDATION', 400);
-const knownErrors = new Set(errorCodeToStatus.keys());
-export const mapErrorCode = (error) => knownErrors.has(error) ? error : 'UNKNOWN';
-export const mapErrorStatus = (error) => errorCodeToStatus.get(error) ?? 500;
