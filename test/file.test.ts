d279d86f7749c0b9f9739b380e84b7fbd0a13d9a
Amirreza Mahyari <amizamahyari@gmail.com>
2023-03-12 22:52:37 +0330

    📘 test: file upload

--- test/file.test.ts
+++ test/file.test.ts
@@ -1,47 +1,44 @@
-// import { Elysia, t } from '../src'
+import { Elysia, t } from '../src'
+import { describe, expect, it } from 'bun:test'
+import { upload } from './utils'
 
-// import { describe, expect, it } from 'bun:test'
-// import { upload } from './utils'
+const app = new Elysia()
+	.post('/single', ({ body: { file } }) => file.size, {
+		schema: {
+			body: t.Object({
+				file: t.File()
+			})
+		}
+	})
+	.post(
+		'/multiple',
+		({ body: { files } }) => files.reduce((a, b) => a + b.size, 0),
+		{
+			schema: {
+				body: t.Object({
+					files: t.Files()
+				})
+			}
+		}
+	)
+	.listen(8080)
 
-// const app = new Elysia()
-// 	.post('/single', ({ body: { file } }) => file.size, {
-// 		schema: {
-// 			body: t.Object({
-// 				file: t.File()
-// 			})
-// 		}
-// 	})
-// 	.post(
-// 		'/multiple',
-// 		({ body: { files } }) => files.reduce((a, b) => a + b.size, 0),
-// 		{
-// 			schema: {
-// 				body: t.Object({
-// 					files: t.Files()
-// 				})
-// 			}
-// 		}
-// 	)
-// 	.listen(8080)
+describe('File', () => {
+	it('accept single image', async () => {
+    const {request,size} = upload('/single', {
+      file: 'millenium.jpg'
+    })
+		const res = await app.handle(request)    
 
-// describe('File', () => {
-// 	it('accept single image', async () => {
-// 		const response = await app.handle(
-// 			upload('/single', {
-// 				file: 'millenium.jpg'
-// 			})
-// 		)
+		expect(await res.text()).toEqual(size.toString())
+	})
 
-// 		expect(response).toEqual(1)
-// 	})
+	it('accept multiple image', async () => {
+    const {request,size} = upload('/multiple', {
+      files: ['aris-yuzu.jpg', 'aris-yuzu.jpg']
+    })
+		const res = await app.handle(request)
 
-// 	it('accept multiple image', async () => {
-// 		const response = await app.handle(
-// 			upload('/multiple', {
-// 				file: ['aris-yuzu.jpg', 'aris-yuzu.jpg']
-// 			})
-// 		)
-
-// 		expect(response).toEqual(true)
-// 	})
-// })
+		expect(await res.text()).toEqual(size.toString())
+	})
+})
