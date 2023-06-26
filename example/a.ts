b5aa9ca0698a58652e6222897c3c866e755fb5a4
saltyaom <saltyaom@gmail.com>
2023-06-26 23:03:27 +0700

    :blue_book: doc: update example

--- example/a.ts
+++ example/a.ts
@@ -1,49 +1,13 @@
-import { Elysia, t, ws } from '../src'
+import { Elysia, t } from '../src'
+import { swagger } from '@elysiajs/swagger'
 
-const app = new Elysia()
-	.get(
-		'/github/callback',
-		async ({
-			query,
-			query: { code, state },
-			// cookie: { github_oauth_state },
-			// setCookie,
-			set
-		}) => {
-			console.log(query, state)
-
-			// if (state !== github_oauth_state) {
-			// 	set.status = 500
-
-			// 	return 'Invalid state'
-			// }
-
-			// try {
-			// 	const { existingUser, providerUser, createUser } =
-			// 		await ghAuth.validateCallback(code as string)
-
-			// 	const user = existingUser
-			// 		? existingUser
-			// 		: await createUser({
-			// 				username: providerUser.login
-			// 		  })
-
-			// 	const { sessionId } = await lucia.createSession(user.userId)
-
-			// 	setCookie('session', sessionId)
-			// } catch (error) {
-			// 	console.log(error)
-
-			// 	if (state !== github_oauth_state) {
-			// 		set.status = 500
-
-			// 		return 'Something went wrong'
-			// 	}
-			// }
-		}
-	)
+new Elysia()
+	// @ts-ignore
+	.use(swagger)
+	.get('/id/:id', ({ params: { id } }) => id)
+	.post("/", ({ body }) => body, {
+		body: t.Object({
+			hello: t.String()
+		})
+	})
 	.listen(3000)
-
-app.handle(new Request('http://localhost/?name=a'))
-	.then((x) => x.text())
-	.then(console.log)
