dcba09e25b847f12851b0ba9212a1281b850e342
saltyaom <saltyaom@gmail.com>
2023-09-20 20:10:55 +0700

    :wrench: fix: disable remove comment

--- test/units/merge-deep.test.ts
+++ test/units/merge-deep.test.ts
@@ -1,6 +1,8 @@
 import { describe, expect, it } from 'bun:test'
 
+import { Elysia } from '../../src'
 import { mergeDeep } from '../../src'
+import { req } from '../utils'
 
 describe('mergeDeep', () => {
 	it('merge empty object', () => {
@@ -33,7 +35,7 @@ describe('mergeDeep', () => {
 		})
 	})
 
-	it('Maintain overlapping class', () => {
+	it('maintain overlapping class', () => {
 		class Test {
 			readonly name = 'test'
 
@@ -43,9 +45,39 @@ describe('mergeDeep', () => {
 		}
 
 		const target = { key1: Test }
-		const source = { key1: Test }
+		const source = { key2: Test }
 
 		const result = mergeDeep(target, source)
 		expect(result.key1).toBe(Test)
+	})
+
+	it('maintain overlapping class in instance', async () => {
+		class DbConnection {
+			health() {
+				return 'ok'
+			}
+
+			getUsers() {
+				return []
+			}
+		}
+
+		const dbPlugin = new Elysia({ name: 'db' }).decorate(
+			'db',
+			new DbConnection()
+		)
+
+		const userRoutes = new Elysia({ prefix: '/user' })
+			.use(dbPlugin)
+			.get('', ({ db }) => db.getUsers())
+
+		const app = new Elysia()
+			.use(dbPlugin)
+			.use(userRoutes)
+			.get('/health', ({ db }) => db.health())
+
+		const response = await app.handle(req('/health')).then((x) => x.text())
+
+		expect(response).toBe('ok')
 	})
 })
