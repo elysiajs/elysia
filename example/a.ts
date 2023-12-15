7e9300a4d53476af961793a8fb8cab63fdbbe98e
saltyaom <saltyaom@gmail.com>
2023-12-15 15:44:00 +0700

    :tada: feat: publish script

--- example/a.ts
+++ example/a.ts
@@ -5,12 +5,6 @@ const app = new Elysia()
 	.get('/', Bun.file('test/kyuukurarin.mp4'))
 	.compile()
 
-// console.log(app.fetch.toString())
-
-// console.log(app)
-
-// console.log(app.routes[0].composed?.toString())
-
 app.handle(req('/'))
 	.then((x) => x.headers)
 	.then(console.log)
