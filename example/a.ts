57cc1a912fe587813de5d3acd29c147624ef87d3
saltyaom <saltyaom@gmail.com>
2024-05-02 16:17:24 +0700

    :tada: release: 1.0.16

--- example/a.ts
+++ example/a.ts
@@ -1,33 +1,4 @@
 import { Elysia } from '../src'
-import { rateLimit } from 'elysia-rate-limit'
-import { req } from '../test/utils'
-import { serverTiming } from '@elysiajs/server-timing'
 
-const app = new Elysia()
-	.use((app) => {
-		app.use(
-			// @ts-expect-error
-			serverTiming()
-		)
-
-		app.use(
-			// @ts-expect-error
-			rateLimit({
-				max: 3
-			})
-		)
-
-		return app
-	})
-	.get('/', 'hello')
-	.listen(3000)
-
-await Promise.all([
-	app.handle(req('/')),
-	app.handle(req('/')),
-	app.handle(req('/'))
-])
-
-app.handle(req('/'))
-	.then((x) => x.text())
-	.then(console.log)
+new Elysia()
+  .listen(3000)
