110e172223204066e87b93e75c71db7eb2c46ac9
saltyaom <saltyaom@gmail.com>
2024-07-03 01:06:04 +0700

    :tada: feat: exp.11

--- example/a.ts
+++ example/a.ts
@@ -2,6 +2,11 @@ import { Elysia, replaceSchemaType, t } from '../src'
 import { TypeCompiler } from '../src/type-system'
 import { req } from '../test/utils'
 
+new Elysia().model('a', t.String()).model((x) => ({
+	...x,
+	b: x.a
+}))
+
 console.log(
 	replaceSchemaType(t.String(), {
 		from: t.String(),
