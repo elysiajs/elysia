b3f8a5b5041b7d6ee157fb92033d5a3b5a97821d
李季 <2943102883@qq.com>
2024-07-13 13:24:16 +0800

    Port can be dynamically configured

--- src/index.ts
+++ src/index.ts
@@ -5227,7 +5227,7 @@ export default class Elysia<
 	 * ```
 	 */
 	listen = (
-		options: string | number | Partial<Serve>,
+		options: string | number | ((data: Singleton['decorator']) => number | string) | Partial<Serve>,
 		callback?: ListenCallback
 	) => {
 		if (typeof Bun === 'undefined')
@@ -5242,6 +5242,9 @@ export default class Elysia<
 				throw new Error('Port must be a numeric value')
 
 			options = parseInt(options)
+		}
+		if (typeof options === 'function') {
+			options = options(this.decorator)
 		}
 
 		const fetch = this.fetch
