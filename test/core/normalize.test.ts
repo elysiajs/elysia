41d12e1d186f85e90bd960908f662b96df01711e
m1212e <14091540+m1212e@users.noreply.github.com>
2024-07-15 23:16:46 +0200

    🐞 fix: implement normalization support for class instances with getter functions

--- test/core/normalize.test.ts
+++ test/core/normalize.test.ts
@@ -113,9 +113,12 @@ describe('Normalize', () => {
 				}
 			},
 			{
-				response: t.Object({
-					hello: t.String()
-				}, { additionalProperties: true })
+				response: t.Object(
+					{
+						hello: t.String()
+					},
+					{ additionalProperties: true }
+				)
 			}
 		)
 
@@ -124,6 +127,87 @@ describe('Normalize', () => {
 		expect(response).toEqual({
 			hello: 'world',
 			a: 'b'
+		})
+	})
+
+	it('normalize response with getter fields on class', async () => {
+		const app = new Elysia({
+			normalize: true
+		}).get(
+			'/',
+			() => {
+				class MyTest {
+					constructor(hello: string) {
+						this.one = hello
+						this.two = hello
+					}
+					public one: string
+					public two: string
+
+					get oneGet() {
+						return this.one
+					}
+
+					get twoGet() {
+						return this.two
+					}
+				}
+
+				const res = new MyTest('world')
+				return res
+			},
+			{
+				response: t.Object(
+					{
+						one: t.String(),
+						oneGet: t.String()
+					},
+					{ additionalProperties: false }
+				)
+			}
+		)
+
+		const response = await app.handle(req('/')).then((x) => x.json())
+
+		expect(response).toEqual({
+			one: 'world',
+			oneGet: 'world'
+		})
+	})
+
+	it('normalize response with getter fields on simple object', async () => {
+		const app = new Elysia({
+			normalize: true
+		}).get(
+			'/',
+			() => {
+				return {
+					one: 'world',
+					get oneGet() {
+						return 'world'
+					},
+					two: 'world',
+					get twoGet() {
+						return 'world'
+					}
+				}
+			},
+			{
+				response: t.Object(
+					{
+						one: t.String(),
+						oneGet: t.String()
+					},
+					{ additionalProperties: false }
+				)
+			}
+		)
+
+		const response = await app.handle(req('/')).then((x) => x.json())
+
+		expect(response).toEqual({
+			one: 'world',
+			oneGet: 'world'
 		})
 	})
 })
