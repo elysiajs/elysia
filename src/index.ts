326261b61f4ef9faa8806a5f7b872bd63716df8f
Gabriel Guimarães <gabrielguimoliver@gmail.com>
2024-07-18 19:55:07 -0300

    elysia.route now recognize config as optional param and routehandlers recognize context param as Context properly

--- src/index.ts
+++ src/index.ts
@@ -125,7 +125,8 @@ import type {
 	HigherOrderFunction,
 	ResolvePath,
 	JoinPath,
-	ValidatorLayer
+	ValidatorLayer,
+	RouteConfig
 } from './types'
 
 export type AnyElysia = Elysia<any, any, any, any, any, any, any, any>
@@ -437,7 +438,7 @@ export default class Elysia<
 		path: string,
 		handle: Handler<any, any, any> | any,
 		localHook?: LocalHook<any, any, any, any, any, any>,
-		{ allowMeta = false, skipPrefix = false } = {
+		{ allowMeta = false, skipPrefix = false }: RouteConfig = {
 			allowMeta: false as boolean | undefined,
 			skipPrefix: false as boolean | undefined
 		}
@@ -4522,11 +4523,8 @@ export default class Elysia<
 			Definitions['error'],
 			Metadata['macro'],
 			JoinPath<BasePath, Path>
-		> & {
-			config: {
-				allowMeta?: boolean
-			}
-		}
+		> &
+			Partial<{ config: RouteConfig }>
 	): Elysia<
 		BasePath,
 		Scoped,
