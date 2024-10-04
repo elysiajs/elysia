7ca0af8f16c732134bb6f8e3af4c03191c7c0017
Alejandro Berzosa <a.berzosa.iglesias@gmail.com>
2024-10-04 10:54:43 +0200

    add test case

--- test/validator/response.test.ts
+++ test/validator/response.test.ts
@@ -109,6 +109,29 @@ describe('Response Validator', () => {
 		expect(res.status).toBe(200)
 	})
 
+  it('validate nested references', async () => {
+    const job = t.Object({
+      name : t.String()
+    }, { $id: "job" })
+
+    const person = t.Object({
+      name : t.String(),
+      job  : t.Ref(job)
+    })
+
+    const app = new Elysia()
+    .model({ job, person })
+    .get('/', () => ({
+      name : 'sucrose',
+      job: { name: 'alchemist' }
+    }), {
+      response : person
+    })
+
+    const res = await app.handle(req('/'))
+		expect(res.status).toBe(200)
+  })
+
 	it('validate optional', async () => {
 		const app = new Elysia().get(
 			'/',
