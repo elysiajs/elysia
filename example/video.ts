8250fe25186fb426eb81da0e42312447a4cf59c9
Josh Meads <8870827+joshmeads@users.noreply.github.com>
2024-10-24 20:20:31 -0700

    fix examples types

--- example/video.ts
+++ example/video.ts
@@ -1,5 +1,3 @@
-import { Elysia } from 'elysia'
+import { Elysia } from '../src'
 
-new Elysia()
-	.get('/', Bun.file('test/kyuukurarin.mp4'))
-	.listen(3000)
+new Elysia().get('/', Bun.file('test/kyuukurarin.mp4')).listen(3000)
