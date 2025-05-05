72c89db26ff6a8c259897ec55b630b495fb95fc5
saltyaom <saltyaom@gmail.com>
2025-05-05 21:38:04 +0700

    :tada: release: 1.3

--- example/a.ts
+++ example/a.ts
@@ -1,34 +1,9 @@
 import { Elysia, t } from '../src'
 
 const app = new Elysia()
-	.onError(({ code, error }) => {
-		console.error('[error]', error)
-		return { error: { code } }
+	.get('/', ({ request }) => {
+		request.url
 	})
-	.get(
-		'/session',
-		({ error, cookie: { sessionToken } }) => {
-			const refreshed = !!sessionToken.value
-
-			sessionToken.set({
-				value: Math.random().toString(36).substring(2, 8),
-				maxAge: 1000 * 60 * 60 * 24 * 7
-			})
-
-			if (refreshed) throw error('Unauthorized')
-
-			return sessionToken.value
-		},
-		{
-			cookie: t.Cookie(
-				{ sessionToken: t.Optional(t.String()) },
-				{
-					sign: ['sessionToken'],
-					secrets: 'my-secret'
-				}
-			)
-		}
-	)
 	.listen(3000)
 
-console.log(app.routes[0].compile().toString())
+// console.log(app.routes[0].compile().toString())
