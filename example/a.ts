8fa088e5689ec05adf789ae7513246dc0b9327a8
saltyaom <saltyaom@gmail.com>
2025-06-16 19:22:14 +0700

    :wrench: fix: property status does not exist onError function

--- example/a.ts
+++ example/a.ts
@@ -1,11 +1,5 @@
 import { Elysia, t } from '../src'
 import homepage from './index.html'
 
-new Elysia()
-	.onError(({ status }) => {
-		console.log(status(400, 2))
-	})
-	.get('/', () => {
-		throw new Error('This is an error')
-	})
-	.listen(3000, (server) => console.log(`Running on ${server.url}`))
+new Elysia({ aot: false })
+	.post('/test', () => 'Hello World', { body: t.Object({}) })
