012e58c36f93f0288228dbe357c7ac0643e03bc4
saltyaom <saltyaom@gmail.com>
2025-10-05 07:38:05 +0700

    :tada: feat: optimize animation

--- example/a.ts
+++ example/a.ts
@@ -1,31 +1,21 @@
-import { Elysia, t } from '../src'
-import { parseCookie } from '../src/cookies'
-import { req } from '../test/utils'
+import { Elysia } from 'elysia'
 
-console.log(getLastBalancedParenIndex(`a({ hello: a() }).thing`))
+class YourError extends Error {
+	status = 418
 
-function getLastBalancedParenIndex(input: string): number {
-	let depth = 0
-	let lastBalancedIndex = -1
-
-	for (let i = 0; i < input.length; i++) {
-		const char = input[i]
-
-		if (char === '(') {
-			depth++
-		} else if (char === ')') {
-			if (depth > 0) {
-				depth--
-				// when depth goes back to 0, it's a balanced pair
-				if (depth === 0) {
-					lastBalancedIndex = i
-				}
-			} else {
-				// Unbalanced closing bracket
-				return -1
-			}
-		}
+	constructor(message: string) {
+		super(message)
 	}
-
-	return lastBalancedIndex
 }
+
+new Elysia()
+	.error({
+		"YOUR_ERROR": YourError
+	})
+	.onError(({ code }) => {
+		return "OHi there"
+	})
+	.get('/', () => {
+		throw new YourError("A")
+	})
+	.listen(3000)
