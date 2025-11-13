58d9659cb89e67f8062905fa7678d37f829b1f65
saltyaom <saltyaom@gmail.com>
2025-11-13 11:26:05 +0700

    :tada: feat: update changelog

--- example/a.ts
+++ example/a.ts
@@ -1,16 +1,22 @@
 import { Elysia, status, t } from '../src'
 
-status(401)
-
-const app = new Elysia()
-	.macro({
-		multiple: {
-			resolve({ status }) {
-				if (Math.random() > 0.5) return status(401)
-				return status(403)
+const auth = (app: Elysia) =>
+	app.derive(({ headers, status }) => {
+		try {
+			const token = headers['authorization']?.replace('Bearer ', '') || ''
+			return {
+				isAuthenticated: true
 			}
+		} catch (e) {
+			const error = e as Error
+			console.error('Authentication error:', error.message)
+			return status(401, 'Unauthorized')
 		}
 	})
-	.get('/multiple', () => 'Ok', { multiple: true })
 
-app['~Routes']['multiple']['get']['response']
+const app = new Elysia()
+	.use(auth)
+	.get('/', ({ isAuthenticated }) => isAuthenticated)
+	.listen(5000)
+
+app['~Routes']
