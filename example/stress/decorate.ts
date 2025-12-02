708577623e77c11cdebdef679199198de101e5d2
saltyaom <saltyaom@gmail.com>
2025-12-02 21:15:03 +0700

    :blue_book: chore: add decorate stress test

--- example/stress/decorate.ts
+++ example/stress/decorate.ts
@@ -27,5 +27,5 @@ Bun.gc(true)
 
 const memoryAfter = process.memoryUsage().heapTotal / 1024 / 1024
 console.log(+(memoryAfter - memory).toFixed(2), 'MB memory used')
-console.log(+(t2 - t1).toFixed(2), 'ms')
-console.log(+(t2 - t1 / total * sub).toFixed(2), 'ms')
+console.log('total', +(t2 - t1).toFixed(2), 'ms')
+console.log(+((t2 - t1) / (total * sub)).toFixed(6), 'decoration/ms')
