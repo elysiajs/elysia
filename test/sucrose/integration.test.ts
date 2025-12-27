1004ce8c22a5fcf173d34dfe06d7cbe4b36d1b7b
Akim Mamedov <akim99999999@gmail.com>
2025-12-27 22:01:15 +0700

    add test for sucrose GC manager

---
+++ test/sucrose/integration.test.ts
@@ -0,0 +1,21 @@
+import { describe, it } from 'bun:test'
+
+describe('Integration', () => {
+	it(
+		'Allows process to finish',
+		async () => {
+			const res = Bun.spawn({
+				cmd: [
+					'bun',
+					'-e',
+					"import { Elysia } from './src'; new Elysia().onBeforeHandle(() => { });"
+				]
+			})
+
+			await res.exited
+		},
+		{
+			timeout: 100
+		}
+	)
+})
