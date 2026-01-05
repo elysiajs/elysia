669245350eb47610fa084822dab117f96e0d00ad
saltyaom <saltyaom@gmail.com>
2026-01-05 07:43:35 +0700

    :wrench: fix: add ValueErrorWithSummary

--- example/a.ts
+++ example/a.ts
@@ -1,14 +1,43 @@
 import { Elysia, t } from '../src'
-import { z } from 'zod'
-import { req } from '../test/utils'
 
-export const app = new Elysia()
-	.ws('/', {
-		open(ws) {
-			ws.subscribe('a')
-		},
-		message(a) {
-			console.log(a.subscriptions)
-		}
-	})
-	.listen(3000)
+export const errorHandler = new Elysia({ name: "error-handler" })
+    .onError(({ code, error, status }) => {
+        switch (code) {
+            case "VALIDATION": {
+            	const allErrors = error.detail
+
+                return status(422, {
+                    status: 422,
+                    message: error.valueError?.message,
+                    details: {
+                        location: error.type,
+                        rejected_value: error.value,
+                        expected: error.expected,
+                    },
+                })
+            }
+            case "NOT_FOUND": {
+                return status(404, {
+                    status: 404,
+                    message: "Route not found",
+                })
+            }
+            default: {
+                const statusCode =
+                    "status" in error
+                        ? (typeof error.status === "number" ? error.status : Number(error.status)) || 500
+                        : 500
+
+                let errMsg = "An error occurred"
+                if (error instanceof Error && error.message) {
+                    errMsg = error.message
+                }
+
+                return status(statusCode, {
+                    status: statusCode,
+                    message: errMsg,
+                })
+            }
+        }
+    })
+    .as("scoped")
