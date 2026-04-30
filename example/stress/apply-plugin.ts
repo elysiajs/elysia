a39022a43e346f9a5087c6170243abe403897112
saltyaom <saltyaom@gmail.com>
2026-04-30 13:06:36 +0700

    :tada: feat: rename event convention, scope + type, optimize handler registration

--- example/stress/apply-plugin.ts
+++ example/stress/apply-plugin.ts
@@ -7,7 +7,7 @@ const plugins = new Array(total)
 for (let i = 0; i < total; i++)
 	plugins[i] = new Elysia().get(`/${i}`, () => 'ok')
 
-const stop = profile('Elysia 2α apply 100k plugins w/ 1 route')
+const stop = profile('Elysia 2α start 100k plugins w/ 1 route')
 const app = new Elysia()
 
 for (let i = 0; i < total; i++)
